45629590e8397be3e555d484c4f0ea075354c0e4
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2025-04-07 16:52:16 -0600

    crear ruta con middlewares para inicio de sesion

--- Auth/Rutas/Rutas_individuales/inicioSesion.routes.js
+++ Auth/Rutas/Rutas_individuales/inicioSesion.routes.js
@@ -2,5 +2,13 @@ const express = require("express");
 const revisarApiKey = require("../../../util/middlewares/revisarApiKey");
 const ruteador = express.Router();
 const controlador = require("../../Controladores/inicioSesion.controller");
+const validarNoSql = require("../../../util/middlewares/validarInjeccionNoSql");
 
-ruteador.post("/login", revisarApiKey("x-api-key"), controlador.inicioSesion);
+ruteador.post(
+  "auth/login",
+  validarNoSql,
+  revisarApiKey("x-api-key"),
+  controlador.inicioSesion
+);
+
+module.exports = ruteador;
--- Auth/Rutas/indexAutenticacion.routes.js
+++ Auth/Rutas/indexAutenticacion.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const ruteador = express.Router();
 const rutasSesion = require("./Rutas_individuales/autenticacionSesion.routes");
+const rutasLogin = require("./Rutas_individuales/inicioSesion.routes");
 
 ruteador.use("/api", rutasSesion);
+ruteador.use("/api");
 
 module.exports = ruteador;
