fbc06f5b5e74a032193cd9360f9ce89d3275e42d
Angeltrek <angeltrek007@gmail.com>
2025-04-13 12:46:00 -0600

    Agregar clientes asociados a la autenticacion

--- Utilidades/Servicios/correrQuery.js
+++ Utilidades/Servicios/correrQuery.js
@@ -1,40 +1,25 @@
-const conexion = require("@altertex/util/bd/db");
-
 /**
- * Ejecuta una consulta SQL en la base de datos y devuelve los resultados.
- * Si ocurre un error durante la consulta, rechaza la promesa con un mensaje de error.
+ * Ejecuta una consulta SQL utilizando la conexión a la base de datos.
  *
  * @async
- * @function correrQuery
- * @param {string} query - La consulta SQL a ejecutar.
- * @param {Array} [parametros=[]] - Los parámetros a incluir en la consulta SQL.
+ * @function
+ * @param {string} query - Consulta SQL a ejecutar.
+ * @param {Array} [params=[]] - Parámetros para la consulta preparada.
+ * @returns {Promise<any>} Promesa que se resuelve con los resultados de la consulta o se rechaza con un error.
  *
- * @returns {Promise<Array>} Una promesa que se resuelve con los resultados de la consulta.
- *
- * @throws {Error} - Si el query no es válido, no se encuentran resultados, o si ocurre un error en la ejecución de la consulta.
- * - Error si el `query` no es una cadena de texto.
- * - Error si no se encuentran resultados o si la consulta falla.
+ * @example
+ * const resultados = await runQuery('SELECT * FROM usuarios WHERE id = ?', [1]);
  */
 
-module.exports = async (query, parametros = []) => {
-  return new Promise((resolver, rechazar) => {
-    if (!query || typeof query !== "string") {
-      rechazar(new Error("El query no es válido."));
-      return;
-    }
+const conexion = require("@altertex/util/bd/db");
 
-    conexion.query(query, parametros, (error, resultados) => {
-      if (error) {
-        console.error("Error en la consulta:", error);
-        rechazar(
-          new Error(`Error en la consulta: ${error.message || "Desconocido"}`)
-        );
+module.exports = async (query, params = []) => {
+  return new Promise((resolver, rechazar) => {
+    conexion.query(query, params, (err, results) => {
+      if (err) {
+        rechazar(err);
       } else {
-        if (!resultados || resultados.length === 0) {
-          rechazar(new Error("No se encontraron resultados."));
-        } else {
-          resolver(resultados);
-        }
+        resolver(results);
       }
     });
   });
