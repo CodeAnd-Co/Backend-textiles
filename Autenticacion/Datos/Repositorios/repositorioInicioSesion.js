e2a6b1375e310413dfc2501ae1efccb10596ee18
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2025-04-13 23:37:40 -0600

    arreglar problemas de eslint y mensaje esperado en test

--- Autenticacion/Datos/Repositorios/repositorioInicioSesion.js
+++ Autenticacion/Datos/Repositorios/repositorioInicioSesion.js
@@ -42,7 +42,7 @@ exports.obtenerUsuario = async (correoElectronico) => {
     };
 
     return resultado;
-  } catch (error) {
+  } catch {
     return `Error obteniendo usuario`;
   }
 };
--- Utilidades/Intermediarios/validarYSanitizar.js
+++ Utilidades/Intermediarios/validarYSanitizar.js
@@ -35,9 +35,9 @@ function validarYSanitizar(req, res, next) {
   for (const [llave, valor] of Object.entries(cuerpo)) {
     // Solo aceptamos strings, números o booleanos simples
     if (
-      typeof valor !== "string" &&
-      typeof valor !== "number" &&
-      typeof valor !== "boolean"
+      typeof valor !== "string"
+      && typeof valor !== "number"
+      && typeof valor !== "boolean"
     ) {
       return res
         .status(400)
