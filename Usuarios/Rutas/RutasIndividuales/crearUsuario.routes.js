d1387cd2808d2263084313822095f9c869fddbe0
NicoH00d <a01705857@tec.mx>
2025-04-21 19:21:56 -0600

    Agregar validacion de inyeccion de sql

--- Usuarios/Rutas/RutasIndividuales/crearUsuario.routes.js
+++ Usuarios/Rutas/RutasIndividuales/crearUsuario.routes.js
@@ -4,12 +4,14 @@ const controlador = require("@altertex/usu/ctrl/crearUsuario.controller");
 const revisarApiKey = require("@altertex/util/inter/revisarApiKey");
 const autorizarToken = require("@altertex/util/inter/autorizarToken");
 const verificarPermisos = require("@altertex/util/inter/verificarPermisos");
+const validarYSanitizar = require("@altertex/util/inter/validarYSanitizar");
 
 const PERMISOS = require("@altertex/util/const/permisos");
 const RUTAS = require("@altertex/util/const/rutas");
 
 ruteador.post(
   RUTAS.USUARIOS.CREAR,
+  validarYSanitizar,
   revisarApiKey(),
   autorizarToken,
   verificarPermisos(PERMISOS.CREAR_USUARIO),
