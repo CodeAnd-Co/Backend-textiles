6e36d79921a704739c770fb323c0d3cf2328f467
Rodrigo Antonio Benítez De La Portilla <a01771433@tec.mx>
2025-04-25 16:32:11 -0600

    feature: rutas eliminar evento en index

--- Eventos/Rutas/indexEventos.routes.js
+++ Eventos/Rutas/indexEventos.routes.js
@@ -1,15 +1,16 @@
-const express = require("express");
+const express = require('express');
 const ruteador = express.Router();
-const rutasConsultarListaEventos = require("@altertex/eve/rutasInd/consultarListaEventos.routes");
-
+const rutasConsultarListaEventos = require('@altertex/eve/rutasInd/consultarListaEventos.routes');
+const rutasEliminarEvento = require('@altertex/eve/rutasInd/eliminarEvento.routes');
 /**
  * @module Eventos/Rutas
  * @description Gestor de rutas para el módulo de eventos
  */
 
-const RUTAS = require("@altertex/util/const/rutas");
+const RUTAS = require('@altertex/util/const/rutas');
 
 // Configuración de las rutas específicas para eventos
 ruteador.use(RUTAS.EVENTOS.BASE, rutasConsultarListaEventos);
+ruteador.use(RUTAS.EVENTOS.BASE, rutasEliminarEvento);
 
 module.exports = ruteador;
