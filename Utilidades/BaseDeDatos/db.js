8a8a543761e4d1fd2709d4af7a9c5abbea6cdeb7
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2025-04-25 12:30:01 -0600

    cambiar database para conectarse al puerto

--- Utilidades/BaseDeDatos/db.js
+++ Utilidades/BaseDeDatos/db.js
@@ -19,9 +19,9 @@ const mysql = require('mysql2');
  *
  * @throws {Error} Si ocurre un error al conectar a la base de datos.
  */
-
 const conexion = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
