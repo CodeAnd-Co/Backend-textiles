1c81960efabc809ba66c6d0546291e719bcca4b2
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2025-04-24 23:44:33 -0600

    docs: Añadir comentarios siguiendo el estándar JSDoc en consultarListaUsuarios

--- Usuarios/Datos/Repositorios/repositorioConsultarListaUsuarios.js
+++ Usuarios/Datos/Repositorios/repositorioConsultarListaUsuarios.js
@@ -1,8 +1,23 @@
-//RF02 Super Administrador Consulta Lista de Usuarios - https://codeandco-wiki.netlify.app/docs/proyectos/textiles/documentacion/requisitos/RF2
-
 const correrQuery = require("@altertex/util/ser/correrQuery");
 const CONSULTAS_USUARIOS = require("@altertex/util/const/consultasUsuarios");
 
+/**
+ * Consulta la lista de usuarios en la base de datos.
+ * RF02 - Super Administrador Consulta Lista de Usuarios -
+ * https://codeandco-wiki.netlify.app/docs/proyectos/textiles/documentacion/requisitos/RF2
+ *
+ * @async
+ * @function consultarListaUsuarios
+ *
+ * @returns {Promise<Object[]>} Arreglo de objetos con los datos de los usuarios.
+ * Cada objeto puede incluir propiedades como `id`, `nombre`, `correo`, `rol`, etc.
+ *
+ * @throws {Error} Si ocurre un error durante la ejecución del query a la base de datos.
+ *
+ * @description
+ * Ejecuta una consulta SQL definida en `CONSULTAS_USUARIOS.OBTENER_LISTA` utilizando el
+ * servicio `correrQuery`. Se utiliza para obtener la lista completa de usuarios registrados.
+ */
 exports.consultarListaUsuarios = async () => {
   const query = CONSULTAS_USUARIOS.OBTENER_LISTA;
 
