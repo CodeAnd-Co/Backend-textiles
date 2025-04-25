acb2a09663d25268204e4baf697838eaf8ca7674
Rodrigo Antonio Benítez De La Portilla <a01771433@tec.mx>
2025-04-25 16:13:21 -0600

    deleted: rutasEliminarEvento

--- Eventos/Rutas/indexEventos.routes.js
+++ Eventos/Rutas/indexEventos.routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const ruteador = express.Router();
 const rutasConsultarListaEventos = require("@altertex/eve/rutasInd/consultarListaEventos.routes");
-const rutasEliminarEvento = require("@altertex/eve/rutasInd/eliminarEvento.routes");
 
 /**
  * @module Eventos/Rutas
@@ -12,6 +11,5 @@ const RUTAS = require("@altertex/util/const/rutas");
 
 // Configuración de las rutas específicas para eventos
 ruteador.use(RUTAS.EVENTOS.BASE, rutasConsultarListaEventos);
-ruteador.use(RUTAS.EVENTOS.BASE, rutasEliminarEvento);
 
 module.exports = ruteador;
