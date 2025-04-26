eb90c813ec6045c1ad4018ff2e5ef6bf8d3501a1
Rodrigo Antonio Benítez De La Portilla <a01771433@tec.mx>
2025-04-25 22:45:01 -0600

    feat: se quito duplicados

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-require("module-alias/register");
-require("@altertex/config/dotenv");
+require('module-alias/register');
+require('@altertex/config/dotenv');
 
 //Importaciones de librerias
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const swaggerJSDoc = require("swagger-jsdoc");
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const swaggerJSDoc = require('swagger-jsdoc');
 
 //Importaciones de configuracion
-const corsOptions = require("@altertex/config/corsOptions");
-const opcionesSwagger = require("@altertex/config/swagger");
-const swaggerUI = require("swagger-ui-express");
+const corsOptions = require('@altertex/config/corsOptions');
+const opcionesSwagger = require('@altertex/config/swagger');
+const swaggerUI = require('swagger-ui-express');
 
 //Importaciones de rutas
-const rutasAutenticacion = require("@altertex/aut/rutas/indexAutenticacion.routes");
-const rutasUsuarios = require("@altertex/usu/rutas/indexUsuarios.routes");
-const rutasCategorias = require("@altertex/cat/rutas/indexCategorias.routes");
-const rutasEventos = require("@altertex/eve/rutas/indexEventos.routes");
-const rutasProductos = require("@altertex/pro/rutas/indexProductos.routes");
-const rutasEmpleados = require("@altertex/emp/rutas/indexEmpleados.routes");
-const rutasClientes = require("@altertex/cli/rutas/indexClientes.routes");
+const rutasAutenticacion = require('@altertex/aut/rutas/indexAutenticacion.routes');
+const rutasUsuarios = require('@altertex/usu/rutas/indexUsuarios.routes');
+const rutasCategorias = require('@altertex/cat/rutas/indexCategorias.routes');
+const rutasEventos = require('@altertex/eve/rutas/indexEventos.routes');
+const rutasProductos = require('@altertex/pro/rutas/indexProductos.routes');
+const rutasEmpleados = require('@altertex/emp/rutas/indexEmpleados.routes');
+const rutasClientes = require('@altertex/cli/rutas/indexClientes.routes');
 const rutasRoles = require('@altertex/rol/rutas/indexRoles.routes');
-const rutasCuotas = require("@altertex/cuota/rutas/indexCuotas.routes");
-const rutasCategorias = require("@altertex/cat/rutas/indexCategorias.routes");
-const RUTAS = require("@altertex/util/const/rutas");
+const rutasCuotas = require('@altertex/cuota/rutas/indexCuotas.routes');
+const RUTAS = require('@altertex/util/const/rutas');
 
 //Importaciones de CRON jobs
-const cronCuotas = require("@altertex/CRON/ctrl/actualizarCuotaSet.controller");
+const cronCuotas = require('@altertex/CRON/ctrl/actualizarCuotaSet.controller');
 
 const puerto = process.env.PORT || 5000;
 
@@ -53,6 +52,5 @@ app.use(RUTAS.API, rutasEventos);
 const swaggerSpec = swaggerJSDoc(opcionesSwagger);
 app.use(RUTAS.API_DOCS, swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.listen(puerto, () =>
-  console.log(
-    `Servidor corriendo en puerto ${puerto} [${process.env.NODE_ENV}]`
-  ));
+  console.log(`Servidor corriendo en puerto ${puerto} [${process.env.NODE_ENV}]`)
+);
