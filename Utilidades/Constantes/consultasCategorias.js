6e9d96b863532e0ef736010054f15f429b51ad3f
PAOLA MARIA garrido <you@example.com>
2025-04-27 11:34:20 -0600

    fix: Cambiar los nombres de las tablas a minusculas

--- Utilidades/Constantes/consultasCategorias.js
+++ Utilidades/Constantes/consultasCategorias.js
@@ -7,11 +7,11 @@ module.exports = {
       COUNT(p.idProducto) AS cantidadProductos,
       p.idCliente
     FROM 
-      CATEGORIA c
+      categoria c
     JOIN 
-      CATEGORIA_PRODUCTO cp ON c.idCategoria = cp.idCategoria
+      categoria_producto cp ON c.idCategoria = cp.idCategoria
     JOIN 
-      PRODUCTO p ON cp.idProducto = p.idProducto
+      producto p ON cp.idProducto = p.idProducto
     WHERE 
       p.idCliente = ?
     GROUP BY 
