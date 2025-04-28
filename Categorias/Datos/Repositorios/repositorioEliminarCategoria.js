3069aaca463e53360766a27c99f6311f3c96394c
angieriosc <A01705651@tec.mx>
2025-04-27 19:15:20 -0600

    Correcciones eslint

--- Categorias/Datos/Repositorios/repositorioEliminarCategoria.js
+++ Categorias/Datos/Repositorios/repositorioEliminarCategoria.js
@@ -1,45 +1,42 @@
-const correrQuery = require("@altertex/util/ser/correrQuery");
-const CONSULTAS_CATEGORIAS = require("@altertex/util/const/consultasCategorias");
+const correrQuery = require('@altertex/util/ser/correrQuery');
+const CONSULTAS_CATEGORIAS = require('@altertex/util/const/consultasCategorias');
 
 /**
- * Elimina la relación entre productos y una categoría, y/o la categoría en sí, de la base de datos.
- * RF[50] - Elimina categoría de productos - https://codeandco-wiki.netlify.app/docs/proyectos/textiles/documentacion/requisitos/RF50
+ * Elimina la relación entre productos y una categoría.
  *
  * @async
  * @function eliminarProductoCategoria
  * @param {number} idCategoria - ID de la categoría a desvincular de productos.
- * @returns {Promise<Object>} Objeto de resultado de la operación MySQL (por ejemplo, `affectedRows`).
+ * @returns {Promise<object>} Objeto de resultado de la operación MySQL (por ejemplo, `affectedRows`).
  * @throws {Error} Si ocurre un error durante la ejecución del query para eliminar la relación.
- *
- * @async
- * @function eliminarCategoria
- * @param {number} idCategoria - ID de la categoría a eliminar completamente de la base de datos.
- * @returns {Promise<Object>} Objeto de resultado de la operación MySQL (por ejemplo, `affectedRows`).
- * @throws {Error} Si ocurre un error durante la ejecución del query para eliminar la categoría.
- *
- * @description
- * Utiliza `correrQuery` para ejecutar las consultas definidas en `CONSULTAS_CATEGORIAS.ELIMINAR_CATEGORIA`.
- * Se espera que la eliminación de categoria_producto se haga antes de eliminar la categoría en sí.
  */
-
 exports.eliminarProductoCategoria = async (idCategoria) => {
   const query = CONSULTAS_CATEGORIAS.ELIMINAR_CATEGORIA_PRODUCTO;
   try {
     const resultado = await correrQuery(query, [idCategoria]);
     return resultado;
   } catch (error) {
-    console.error("Error al eliminar categoría:", error);
+    console.error('Error al eliminar categoría:', error);
     throw error;
   }
 };
 
+/**
+ * Elimina una categoría de la base de datos.
+ *
+ * @async
+ * @function eliminarCategoria
+ * @param {number} idCategoria - ID de la categoría a eliminar.
+ * @returns {Promise<{affectedRows: number}>} Resultado de la operación con el número de filas afectadas.
+ * @throws {Error} Si ocurre un error durante la operación.
+ */
 exports.eliminarCategoria = async (idCategoria) => {
   const query = CONSULTAS_CATEGORIAS.ELIMINAR_CATEGORIA;
   try {
     const resultado = await correrQuery(query, [idCategoria]);
     return resultado;
   } catch (error) {
-    console.error("Error al eliminar categoría:", error);
+    console.error('Error al eliminar categoría:', error);
     throw error;
   }
 };
