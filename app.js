194303c8fcb1e73d016c95d2914e47c8a28d086c
Rodrigo Antonio Benítez De La Portilla <a01771433@tec.mx>
2025-04-29 16:47:07 -0600

    fix:app.js

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const rutasClientes = require('@altertex/cli/rutas/indexClientes.routes');
 const rutasRoles = require('@altertex/rol/rutas/indexRoles.routes');
 const rutasCuotas = require('@altertex/cuota/rutas/indexCuotas.routes');
 const rutasCategorias = require('@altertex/cat/rutas/indexCategorias.routes');
+const rutasEventos = require('@altertex/eve/rutas/indexEventos.routes');
 const RUTAS = require('@altertex/util/const/rutas');
 
 //Importaciones de CRON jobs
@@ -52,4 +53,6 @@ app.use(RUTAS.API, rutasEventos);
 //Configuracion de swaggerUI
 const swaggerSpec = swaggerJSDoc(opcionesSwagger);
 app.use(RUTAS.API_DOCS, swaggerUI.serve, swaggerUI.setup(swaggerSpec));
-app.listen(puerto, () => console.log(`Servidor corriendo en puerto ${puerto} [${process.env.NODE_ENV}]`));
+app.listen(puerto, () =>
+  console.log(`Servidor corriendo en puerto ${puerto} [${process.env.NODE_ENV}]`)
+);
