4d4aec49dd6e426c492f5788bbb0e8141a36d662
ArturoSanRod <a01275427@tec.mx>
2025-05-01 15:35:01 -0600

    Fix de errores

--- Roles/Datos/Repositorios/obtenerOpcionesRolRepositorio.js
+++ Roles/Datos/Repositorios/obtenerOpcionesRolRepositorio.js
@@ -1,9 +1,11 @@
+const correrQuery = require('@altertex/util/ser/correrQuery');
+
 /**
  * Obtiene la lista de permisos disponibles desde la base de datos.
  *
  * @async
  * @function obtenerPermisos
- * @returns {Promise<Array<object>>} Arreglo de objetos con los permisos disponibles.
+ * @returns {Promise<Array<{id: number, nombre: string}>>} Arreglo de objetos con los permisos disponibles.
  */
 exports.obtenerPermisos = async () => {
   const consulta = 'SELECT idPermiso AS id, nombre FROM permiso';
