818005e6bb64a739c37a13dce9981c0641ff7dd4
Angeltrek <angeltrek007@gmail.com>
2025-05-02 13:11:26 -0600

    fix: Arreglar errores del linter

--- app.js
+++ app.js
@@ -54,5 +54,4 @@ app.use(RUTAS.API, rutasCategorias);
 const swaggerSpec = swaggerJSDoc(opcionesSwagger);
 app.use(RUTAS.API_DOCS, swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.listen(puerto, () =>
-  console.log(`Servidor corriendo en puerto ${puerto} [${process.env.NODE_ENV}]`)
-);
+  console.log(`Servidor corriendo en puerto ${puerto} [${process.env.NODE_ENV}]`));
