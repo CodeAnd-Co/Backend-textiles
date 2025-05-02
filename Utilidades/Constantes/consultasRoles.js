021b7dfb43d19e58730c26f8bf0ed6116eeaa85f
Hiram <147564077+Hiram10tec@users.noreply.github.com>
2025-05-02 17:15:45 -0600

    hotfix(roles): corregir consulta de roles

--- Utilidades/Constantes/consultasRoles.js
+++ Utilidades/Constantes/consultasRoles.js
@@ -29,8 +29,8 @@ module.exports = {
    */
   OBTENER_LISTA: `
     SELECT r.idRol, r.nombre, r.descripcion, COUNT(ur.idUsuario) AS totalUsuarios
-    FROM Rol r
-    LEFT JOIN Usuario_Rol ur ON r.idRol = ur.idRol
+    FROM rol r
+    LEFT JOIN usuario_rol ur ON r.idRol = ur.idRol
     GROUP BY r.idRol;
   `,
   VERIFICAR_NOMBRE_ROL: `
