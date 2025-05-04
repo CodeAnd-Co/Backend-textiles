1e9c316482ae2f4dfb21bcdc7987f785fabaddc5
angieriosc <A01705651@tec.mx>
2025-05-04 00:35:38 -0600

    Feat: Eliminar pedidos routes

--- Pedidos/Rutas/indexPedidos.routes.js
+++ Pedidos/Rutas/indexPedidos.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ruteador = express.Router();
 const rutasObtenerPedidos = require('@altertex/pedidos/rutasInd/obtenerPedidos.routes');
+const rutasEliminarPedido = require('@altertex/pedidos/rutasInd/eliminarPedidos.routes');
 const RUTAS = require('@altertex/util/const/rutas');
 
 ruteador.use(RUTAS.PEDIDOS.BASE, rutasObtenerPedidos);
