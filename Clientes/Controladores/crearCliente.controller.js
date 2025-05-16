4e08c9db4fc42b48d2f82ef25a8dce8240e39fdc
Diego Alfaro Pinto <a01709971@tec.mx>
2025-05-16 00:59:08 -0600

    feat: arreglar error de lint

--- Clientes/Controladores/crearCliente.controller.js
+++ Clientes/Controladores/crearCliente.controller.js
@@ -66,10 +66,10 @@ exports.crearCliente = async (req, res) => {
         resultadoCliente.insertId
       );
       if (
-        resultadoCliente.insertId &&
-        resultadoVincular.affectedRows &&
-        resultadoImagen.insertId &&
-        resultado.affectedRows === 1
+        resultadoCliente.insertId
+        && resultadoVincular.affectedRows
+        && resultadoImagen.insertId
+        && resultado.affectedRows === 1
       ) {
         return res.status(201).json({ mensaje: MENSAJES.CLIENTE_CREADO.mensaje });
       }
