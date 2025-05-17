247ca70df183eecc0e44975ac08caced241caf77
Ubuntu <ubuntu@ip-172-31-90-178.ec2.internal>
2025-05-17 10:06:56 +0000

    hotfix: arreglar cors para que permita los metodos necesarios

--- Configuracion/corsOptions.js
+++ Configuracion/corsOptions.js
@@ -1,5 +1,6 @@
 module.exports = {
   origin: [process.env.LOCAL_URL, process.env.DEPLOYED_URL],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
