f5dc496a1c69a6b2ca7d2777132c1c150d92bfe1
NicoH00d <a01705857@tec.mx>
2025-05-23 16:49:25 -0600

    fix(importarCSV): Aumentar limite de csv a 5mb

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ const puerto = process.env.PORT || 5000;
 
 //Configuracion de aplicacion express
 const app = express();
-app.use(express.json({ limit: '3mb' }));
-app.use(express.urlencoded({ limit: '3mb', extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
