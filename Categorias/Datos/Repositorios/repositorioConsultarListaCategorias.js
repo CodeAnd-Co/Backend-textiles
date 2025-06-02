03689497f0e4713a10669fe6b48ddce66dc1807f
PAOLA MARIA garrido <you@example.com>
2025-06-02 16:31:03 -0600

    fix: actualizar comentarios

--- Categorias/Datos/Repositorios/repositorioConsultarListaCategorias.js
+++ Categorias/Datos/Repositorios/repositorioConsultarListaCategorias.js
@@ -11,10 +11,6 @@ const CONSULTAS_CATEGORIAS = require('@altertex/util/const/consultasCategorias')
  *
  * @throws {Error} Lanza un error si ocurre un fallo al ejecutar la consulta a la base de datos.
  *
- * @description
- * Esta función ejecuta una consulta SQL definida en `CONSULTAS_CATEGORIAS.OBTENER_CATEGORIAS_CON_PRODUCTOS`
- * utilizando el helper `correrQuery`. Se utiliza en el RF[47] para listar categorías por cliente.
- *
  * @see [RF47 - Documentación de requisitos](https://codeandco-wiki.netlify.app/docs/proyectos/textiles/documentacion/requisitos/RF47)
  */
 exports.consultarListaCategorias = (idCliente) => {
