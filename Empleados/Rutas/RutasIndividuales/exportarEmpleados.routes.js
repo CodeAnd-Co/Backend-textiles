8bc0fb4a96c7837a1ea0c70db7240dbd0925d346
PAOLA MARIA garrido <you@example.com>
2025-06-03 12:08:52 -0600

    fix: actualizar comentario de swagger

--- Empleados/Rutas/RutasIndividuales/exportarEmpleados.routes.js
+++ Empleados/Rutas/RutasIndividuales/exportarEmpleados.routes.js
@@ -1,20 +1,9 @@
 //RF59 - Exportar Empleados- https://codeandco-wiki.netlify.app/docs/next/proyectos/textiles/documentacion/requisitos/RF59
 
-const express = require('express');
-const ruteador = express.Router();
-const controlador = require('@altertex/emp/ctrl/exportarEmpleados.controller');
-const revisarApiKey = require('@altertex/util/inter/revisarApiKey');
-const autorizarToken = require('@altertex/util/inter/autorizarToken');
-const verificarPermisos = require('@altertex/util/inter/verificarPermisos');
-const PERMISOS = require('@altertex/util/const/permisos');
-const RUTAS = require('@altertex/util/const/rutas');
-const validarYSanitizar = require('@altertex/util/inter/validarYSanitizar');
-const limitePeticionesDiarias = require('@altertex/util/inter/limitePeticiones');
-
 /**
  * @swagger
  * /api/empleados/exportar:
- *   get:
+ *   post:
  *     summary: Exporta la lista completa de empleados en formato CSV.
  *     tags:
  *       - Empleados
@@ -52,26 +41,6 @@ const limitePeticionesDiarias = require('@altertex/util/inter/limitePeticiones')
  *                 mensaje:
  *                   type: string
  *                   example: No hay empleados para exportar.
- *       400:
- *         description: Clave de API inválida o ausente.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 mensaje:
- *                   type: string
- *                   example: API key inválida.
- *       401:
- *         description: Usuario no autenticado o token JWT inválido.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 mensaje:
- *                   type: string
- *                   example: No autorizado.
  *       500:
  *         description: Error interno del servidor al exportar empleados.
  *         content:
@@ -83,6 +52,17 @@ const limitePeticionesDiarias = require('@altertex/util/inter/limitePeticiones')
  *                   type: string
  *                   example: Error al exportar la lista de empleados.
  */
+
+const express = require('express');
+const ruteador = express.Router();
+const controlador = require('@altertex/emp/ctrl/exportarEmpleados.controller');
+const revisarApiKey = require('@altertex/util/inter/revisarApiKey');
+const autorizarToken = require('@altertex/util/inter/autorizarToken');
+const verificarPermisos = require('@altertex/util/inter/verificarPermisos');
+const PERMISOS = require('@altertex/util/const/permisos');
+const RUTAS = require('@altertex/util/const/rutas');
+const validarYSanitizar = require('@altertex/util/inter/validarYSanitizar');
+const limitePeticionesDiarias = require('@altertex/util/inter/limitePeticiones');
 
 ruteador.post(
   RUTAS.EMPLEADOS.EXPORTAR_EMPLEADOS,
