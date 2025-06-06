ba297a8160ae821848cfd19c6f4eee1e026d65e3
Diego Alfaro Pinto <a01709971@tec.mx>
2025-06-06 03:04:42 -0600

    feat: agregar proteccion CSRF

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const swaggerJSDoc = require('swagger-jsdoc');
+const csrf = require('csurf')
 
 //Importaciones de configuracion
 const corsOptions = require('@altertex/config/corsOptions');
@@ -35,10 +36,17 @@ const puerto = process.env.PORT || 5000;
 
 //Configuracion de aplicacion express
 const app = express();
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(express.json({limit: '5mb'}));
+app.use(express.urlencoded({limit: '5mb', extended: true}));
 app.use(cookieParser());
 app.use(cors(corsOptions));
+const proteccionCsrf = csrf({
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict'
+    }
+})
 
 cronCuotas.start();
 
@@ -57,14 +65,25 @@ app.use(RUTAS.API, rutasPedidos);
 app.use(RUTAS.API, rutasEventos);
 app.use(RUTAS.API, rutasPagos);
 
+app.use((req, res, next) => {
+    if (req.method === 'GET' && req.path === '/api/csrf-token') {
+        return next()
+    }
+    proteccionCsrf(req, res, next)
+})
+
+app.get('/api/csrf-token', proteccionCsrf, (req, res) => {
+    res.json({csrfToken: req.csrfToken()});
+});
+
 app.get('/', async (req, res) => {
-  return res
-    .status(200)
-    .json({ mensaje: `Ruta por default Proyecto Text&Lines en ambiente: ${process.env.NODE_ENV}` });
+    return res
+        .status(200)
+        .json({mensaje: `Ruta por default Proyecto Text&Lines en ambiente: ${process.env.NODE_ENV}`});
 });
 
 //Configuracion de swaggerUI
 const swaggerSpec = swaggerJSDoc(opcionesSwagger);
 app.use(RUTAS.API_DOCS, swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.listen(puerto, () =>
-  console.log(`Servidor corriendo en puerto ${puerto} [${process.env.NODE_ENV}]`));
+    console.log(`Servidor corriendo en puerto ${puerto} [${process.env.NODE_ENV}]`));
