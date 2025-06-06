9c7e5e861f075af91a5c6721e650d2afd168a5eb
Rodrigo Antonio Benítez De La Portilla <a01771433@tec.mx>
2025-06-06 04:56:47 -0600

    Feat: Agregar manejo de errores en la actualización de productos

--- Productos/Datos/Repositorios/repositorioActualizarProducto.js
+++ Productos/Datos/Repositorios/repositorioActualizarProducto.js
@@ -55,6 +55,7 @@ exports.actualizarProducto = async (clienteSeleccionado, producto) => {
     const idProducto = resultados.insertId;
     return idProducto;
   } catch (error) {
+    console.error('Error al actualizar el producto:', error);
     return [];
   } finally {
     if (conexion) conexion.release();
